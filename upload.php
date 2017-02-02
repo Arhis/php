839530aff60bd7cc24c7bc40473f90cad9d588bf
Arhis <avanture_boy@mail.ru>
2017-02-02 15:47:16 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -10,35 +10,46 @@ echo <<<_END
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Upload One</title>
-</head>
+    <title>Secure uploading</title>
+<head>
 <body>
-<form method = 'post' action = 'upload_two.php' enctype = 'multipart/form-data'>
-Загрузить: <input type = 'file' name = 'filename' size = '10'><br><br>
-<input type = 'submit' value = 'Загрузить'>
+<form method = 'post' action = 'upload_secure.php' enctype = 'multipart/form-data'>
+Загрузите файл с расширением JPEG, GIF, PNG или TIFF:
+<input type = 'file' name = 'file_name' size = '10'>
+<input type = 'submit' value = 'Загрузить на сервер'>
 </form>
 _END;
 
 if($_FILES)
-{
-    $form_name = $_FILES['filename']['name']; //gettring name of file from form
-    $file_extension = end(explode(".", $form_name)); //devide file name to two parts before and after the dot to extract extension of it
-    
-    /*Giving the name of uploaded file (from 1 to ...)*/
-    $give_name = 1; 
-    
-    /*loop that check of exsistance of the file, if TRUE name changing on 1 one more*/
-    while(file_exists(sprintf("%02s", $give_name).".".$file_extension))
+{    
+    switch ($_FILES['file_name']['type'])
     {
-        ++$give_name;
+        case 'image/jpeg': $extension = 'jpg';  break;
+        case 'image/gif' : $extension = 'gif';  break;
+        case 'image/png' : $extension = 'png';  break;
+        case 'image/tiff': $extension = 'tiff'; break;
+        default :          $extension = '';     break;
     }
     
-    $final_name = sprintf("%02s", $give_name).".".$file_extension;  //we got final name of file
-    move_uploaded_file($_FILES['filename']['tmp_name'], $final_name); //saving file with given name to hard driver
-    $file_size = $_FILES['filename']['size'];
-    
-    echo "Загружаемое изображение: '"
-       . "$final_name' и его размер = $file_size bytes <br>"
-       . "<img src='$final_name'></body></html>"
+    if ($extension)
+    {
+        /*Giving the name of uploaded file (from 1 to ...)*/
+        $give_name = 1; 
+
+        /*loop that check of exsistance of the file, if TRUE name changing on 1 one more*/
+        while(file_exists(sprintf("%02s", $give_name).".".$extension))
+        {
+            ++$give_name;
+        }
+
+        $final_name = sprintf("%02s", $give_name).".".$extension;  //we got final name of file
+        move_uploaded_file($_FILES['file_name']['tmp_name'], $final_name);
+        $file_size = $_FILES['file_name']['size'];
+        echo "Загруженное изображение '$final_name' и его размер: $file_size bytes"
+            ."<br><img src = '$final_name'></body></html>";        
+    }   else {
+        echo "Загрузка не удалась. Неверный формат изображения.";
+        echo "</body></html>";
+    }
 }
 
